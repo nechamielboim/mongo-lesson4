4a818a3b15ebb184002a7a66360da2f3c1d266ae
nechami <nnn@gmail.com>
2024-11-06 01:48:09 +0200

    first file

--- untitled.js
+++ untitled.js
@@ -1,25 +1,5 @@
-////2
-//db.books.find()
-////3
-//db.books.find({ISBN:'1933988797'})
-////4
-//db.books.find({name:/java/},{name})
-////5
-//db.books.find({status:'PUBLISH'},{name,date,_id})
-////6
-//db.books.find({price:{$gt:100}},{name,numPages,ISBN})
-////7
-//db.books.find({name:/c#/})
-////8
-//db.books.find({name:/\a/})
-////9
-//db.books.find({numPages:[10-100]},{name,numPages})
-////10
-//db.books.find({name:/r/})
-////12
-//db.books.find({name:/\e/},{name,_id})
-//11
 use catalog
+//11
 db.books.find({title:/data/i})
 //13
 db.books.find().sort({title:1})
